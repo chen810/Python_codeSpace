8e06b027e87f7a0dd232a642920dbe3c31c41d6e
chen810 <54208899+chen810@users.noreply.github.com>
2021-01-29 03:15:27 +0800

    Default Changelist

--- src/test.py
+++ src/test.py
@@ -1,10 +1,10 @@
-import math
+# from PyQt5.QtCore import *
+import sys
 
+from PyQt5.QtWidgets import *
 
-def fun():
-    print(math.pi)
-    print("OK")
-
-
-if __name__=="__main__":
-    fun()
+if __name__ == "__main__":
+    app = QApplication([])  # 建立应用
+    wo = QWidget()  # 建立窗口
+    wo.show()  # 显示窗口
+    sys.exit(app.exec_())
